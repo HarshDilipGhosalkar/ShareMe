c9f1dd727b9cf2ef990b1ea935ff3dc68f6b88b7
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-10-17 22:10:30 +0530

    grabbed the user details and saved it in sanity database

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from 'react';
+// import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import axios from "axios"
+import { client } from '../client';
 
 const Login = () => {
-  return (
-    <div>login</div>
-  )
-}
+    const navigate = useNavigate();
+    const responseGoogle = (response) => {
+        // localStorage.setItem('user', JSON.stringify(response.profileObj));
+        // const { name, googleId, imageUrl } = response.profileObj;
+        // const doc = {
+        //   _id: googleId,
+        //   _type: 'user',
+        //   userName: name,
+        //   image: imageUrl,
+        // };
+        // client.createIfNotExists(doc).then(() => {
+        //   navigate('/', { replace: true });
+        // });
+        console.log(response);
+    };
+    const login = useGoogleLogin({
+        onSuccess: async respose => {
+            try {
+                const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+                    headers: {
+                        "Authorization": `Bearer ${respose.access_token}`
+                    }
+                })
+                const { name, picture, sub } = res.data;
+                
+                const doc = {
+          _id: sub,
+          _type: 'user',
+          userName: name,
+          image: picture,
+        };
+        console.log(doc);
+        client.createIfNotExists(doc).then(() => {
+            navigate('/', { replace: true });
+          });
+            } catch (err) {
+                console.log(err)
 
-export default Login
+            }
+
+        }
+        
+    });
+    return (
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className=" relative w-full h-full">
+                <video
+                    src={shareVideo}
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className="w-full h-full object-cover"
+                />
+
+                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+                    <div className="p-5">
+                        <img src={logo} width="130px" />
+                    </div>
+
+                    <div className="shadow-2xl">
+
+                        <button
+                            type="button"
+                            className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                            onClick={() => login()}
+
+                        >
+                            <FcGoogle className="mr-4" /> Sign in with google
+                        </button>
+                        {/* <GoogleLogin
+                            onSuccess={credentialResponse => {
+                                var decoded = jwt_decode(credentialResponse.credential);
+                                console.log(decoded);
+                            }}
+                            onError={() => {
+                                console.log('Login Failed');
+                            }}
+                        /> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
